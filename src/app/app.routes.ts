757167e4c1b72e7e2ad45dc8f5a910d1380cffe1
Roosevelt Franklin <roosevelt_franklin@hotmail.com>
2024-04-05 18:07:01 -0300

    rotas ok

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { ContatoComponent } from './contato/contato.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomepageComponent },
+    { path: 'contato', component: ContatoComponent }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
